c27c3f459123139706b9d19d739feef3ade427b3
nirav-chavda <niravdchavda@gmail.com>
2018-12-19 16:12:43 +0530

    Connection promises specified

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,14 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://127.0.0.1:27017/Mongoose',{useNewUrlParser:true});
+mongoose.connect('mongodb://127.0.0.1:27017/Mongoose',{useNewUrlParser:true})
+    .then(() => {
+        console.log("Started");
+    })
+    .catch((err) => {
+        console.log('Error on start: ' + err.stack);
+    }
+);
+
 
 module.exports = { mongoose }; // exporting the object that's why it is surrounded by braces
